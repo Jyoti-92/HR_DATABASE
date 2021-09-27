677387b85d38343fbb1c9e001ed8ac5fd5d59a7c
Jyoti-92 <84705208+Jyoti-92@users.noreply.github.com>
2021-09-27 17:49:36 +0530

    Add files via upload

---
+++ HR_DATABASE.sql
@@ -0,0 +1,17 @@
+Emp_ID,First_Name,Last_Name,Gender
+1001,Sourabh,Pandey,Male
+998,Shuvangi,Koushik,Female
+1003,Jyoti,Samantray,Female
+997,Raji,Patel,Female
+1001,Sourabh,Pandey,Male
+998,Shuvangi,Koushik,Female
+1003,Jyoti,Samantray,Female
+997,Raji,Patel,Female
+999,Swati,Nayak,Female
+1008,Karthik,Panda,Male
+1006,Kirti,Sahu,Female
+1009,shruti,Mandal,Female
+1007,Srejita,Pattnayak,Female
+1111,Anuj,Singh,Male
+1010,Surya,Tripathi,Male
+1100,Shubham,Mahant,Male
